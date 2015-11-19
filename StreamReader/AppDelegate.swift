d779c2d92e5e33430d11fe35d3438a7c56ac8c8a
Louis Melahn <lmelahn@legionaries.org>
2015-11-19 21:28:05 +0100

    Initial Commit

---
+++ StreamReader/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  StreamReader
+//
+//  Created by Louis Melahn on 11/19/15.
+//  Copyright © 2015 Louis Melahn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
