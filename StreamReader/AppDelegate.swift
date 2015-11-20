e9809fef8355c2b2bb733665697e055d947b6e77
Louis Melahn <lmelahn@legionaries.org>
2015-11-20 05:49:15 +0100

    Works, but I am going to try to consolidate the initializations.

--- StreamReader/AppDelegate.swift
+++ StreamReader/AppDelegate.swift
@@ -16,12 +16,37 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        
+        if let sr = StreamReader(path: "/Users/lmelahn/Desktop/randomize_mac_address.sh") {
+            while let line = sr.nextLine() {
+                print(line)
+            }
+        }
+        print("")
+        if let sr = StreamReader(path: "/Users/lmelahn/Desktop/randomize_mac_address.sh") {
+            for line in sr {
+                print(line)
+            }
+        }
+        
+        
+        let task = NSTask()
+        task.launchPath = "/Users/lmelahn/Desktop/EmitNumbersAtIntervals.app"
+
+        let pipe = NSPipe()
+        task.standardOutput = pipe
+            
+        task.launch()
+        if let sr = StreamReader(fileHandle: pipe.fileHandleForReading) {
+            for line in sr {
+                print(line)
+            }
+        }
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
 
 }
 
